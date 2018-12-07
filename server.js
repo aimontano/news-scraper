b01d55f944802f454b3b46e0efbe0a0b083b7508
aimontano <aimorales.montano@gmail.com>
2018-12-06 22:44:45 -0500

    Server for heroku

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+const mongoose = require('mongoose');
+const logger = require('morgan');
 const express = require('express');
 const scraper = require("./scraper.js");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static('public'));
+
+let databaseURI = 'mongodb://localhost/news-scraper';
+
+
+if(process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  // mongoose.connect(databaseURI)
+}
+
+let db = mongoose.connection;
+
+db.on('error', err => console.log("Mongoose Error:", err));
+
+db.once('open', () => console.log("Mongoose connection successful!!"));
+
 app.get('/scrape', (req, res) => {
   let articles = [];
   

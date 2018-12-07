941fb85483ab2bc4697eee2b67d413b368b4ff07
aimontano <aimorales.montano@gmail.com>
2018-12-06 22:01:57 -0500

    Simple view of each article

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,24 @@
 
 const displayArticles = articles => {
   articles.forEach(article => {
-    let item = $('<li>').addClass('collection-item');
-    let title = $('<span>').addClass('title');
-    let p = $('<p>')
+    let item = $('<a>').addClass('collection-item black-text');
+    let title = $('<h5>').addClass('title');
+    let link = $('<a>').attr('href', article.link);
+    let description = $('<p>').text(article.description);
+    let btnSave = $('<a>').addClass('secondary-content');
+    let btnIcon = $('<i>').addClass('material-icons');
+
     title.text(article.title);
+    link.text(article.link);
+
+    btnIcon.text('save');
+    btnSave.append(btnIcon);
 
     item.append(title);
+    item.append(link);
+    item.append(description);
+    item.append(btnSave);
+
     $('#articles').append(item);
   });  
 };

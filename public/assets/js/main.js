4233b5cb4f6de55606e056cebb92a0c15329b593
aimontano <aimorales.montano@gmail.com>
2018-12-06 22:17:01 -0500

    able to load and display articles properly

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,19 @@
 
 const displayArticles = articles => {
   articles.forEach(article => {
-    let item = $('<a>').addClass('collection-item black-text');
-    let title = $('<h5>').addClass('title');
+    let item = $('<a>').addClass('collection-item avatar black-text');
+    let title = $('<h5>');
     let link = $('<a>').attr('href', article.link);
     let description = $('<p>').text(article.description);
-    let btnSave = $('<a>').addClass('secondary-content');
+    let btnSave = $('<button>').addClass('secondary-content btn-floating red');
     let btnIcon = $('<i>').addClass('material-icons');
 
     title.text(article.title);
     link.text(article.link);
 
-    btnIcon.text('save');
+    link.attr('target', '_blank');
+
+    btnIcon.text('bookmark');
     btnSave.append(btnIcon);
 
     item.append(title);

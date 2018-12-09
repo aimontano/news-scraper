377a344397468602a239dacd911213f19fef5d4b
aimontano <aimorales.montano@gmail.com>
2018-12-09 17:24:31 -0500

    Route handle scraping

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,31 @@
+const mongoose = require('mongoose');
 const scraper = require('./../scraper.js');
+
+let databaseURI = 'mongodb://localhost/news-scraper';
+
+if(process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseURI);
+}
+
+let db = mongoose.connection;
+
+// db.on('error', err => console.log("Mongoose Error:", err));
+// db.once('open', () => console.log("Mongoose connection successful!!"));
+
+db = require('./../models');
+
+const saveScrapedArticles = articles => {
+  articles.forEach(article => {
+    db.Article.create(article)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => console.log(err));    
+  });
+};
+
 module.exports = app => {
   app.get('/scrape', (req, res) => {
     let articles = [];
@@ -8,7 +35,12 @@ module.exports = app => {
     });
   
     setTimeout(function(){
+      saveScrapedArticles(articles);
       res.json(articles);
-    }, 1000 * 2)
-  });  
+    }, 1000 * 2);
+  }); 
+  
+  app.get('/articles', (req, body) => {
+
+  });
 }

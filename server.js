8983c1f62d98c05e341478dbacc08a939af4c8ff
aimontano <aimorales.montano@gmail.com>
2018-12-09 17:24:10 -0500

    Moved db handling to routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
 const logger = require('morgan');
 const express = require('express');
-const scraper = require("./scraper.js");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,20 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
-
-let databaseURI = 'mongodb://localhost/news-scraper';
-
-
-if(process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect(databaseURI);
-}
-
-let db = mongoose.connection;
-
-db.on('error', err => console.log("Mongoose Error:", err));
-db.once('open', () => console.log("Mongoose connection successful!!"));
 
 require('./routes/staticRoutes.js')(app);
 require('./routes/apiRoutes.js')(app);
